e1910ac721ced0f924b61d7fdd3b8b133e5fe4d7
Khalim Galimov <khalimgal@gmail.com>
2020-11-17 21:11:18 +0300

    Creating spring mvc app - ZooFarm

---
+++ src/main/java/ru/uennar/StartController.java
@@ -0,0 +1,13 @@
+package ru.uennar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+
+    @GetMapping("/start")
+    public String start(){
+        return "start";
+    }
+}
